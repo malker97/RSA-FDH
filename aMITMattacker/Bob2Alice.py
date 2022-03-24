504aaa6c1630aa0d010024eedb5c7611742b46a7
Zhengmao Zhang <zhengmao@pdx.edu>
2022-03-24 02:09:44 -0700

    尝试实现模拟中间人

---
+++ aMITMattacker/Bob2Alice.py
@@ -0,0 +1,13 @@
+import socket
+
+UDP_IP = "192.168.0.197"
+ALICE_UDP_IP = "192.168.0.163"
+ALICE_UDP_PORT = 5005
+BOB_UDP_PORT = 5006
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, ALICE_UDP_PORT))
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("received message: %s" % data)
+    sock.sendto(data, (ALICE_UDP_IP, ALICE_UDP_PORT))

41a3a49ff03bed7be3499bef9beded411d68eafe
Zhengmao Zhang <zhengmao@pdx.edu>
2022-03-23 23:51:10 -0700

    实现了对明文的签名，发送，接收，验证

--- mima.py
+++ mima.py
@@ -1,6 +1,5 @@
 import socket
 import verify
-import ast
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
@@ -10,6 +9,5 @@ sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("received message: %s" % data)
-    data = data.decode("utf-8") 
-    data = ast.literal_eval(data)
     verify.vertify(data)
+    sock.sendto(data, (UDP_IP, UDP_PORT))
--- send.py
+++ send.py
@@ -1,18 +1,30 @@
 from email.message import Message
+from pydoc import plain
 import socket
 import sys
 import signature
+import ast
+
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
-# MESSAGE = b"Hello, World!"
 
 MESSAGE = sys.argv[1]
+plaintext = MESSAGE
+plaintext = str.encode(plaintext)
 MESSAGE = signature.signature(MESSAGE)
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
 print("message: %s" % MESSAGE)
+data_dict = {
+  "plaintext": plaintext,
+  "signature": MESSAGE,
+}
+
+data_string = str(data_dict)
+data_bin = str.encode(data_string)
+# data_dict = ast.literal_eval(data_string)
 
 sock = socket.socket(socket.AF_INET, # Internet
                         socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(data_bin, (UDP_IP, UDP_PORT))
